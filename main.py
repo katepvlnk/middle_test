dab351a4bd82469b8c1f14c1a66a448a15cd627e
Kate <katerynapavlenko01@gmail.com>
2026-04-08 14:09:38 +0300

    Add count_sentences function

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def count_words(text):
     words = re.split(r'[ ,;:]+', text.strip())
     words = [w for w in words if w]
     return len(words)
+
+def count_sentences(text):
+    sentences = re.split(r'\.{3}|[.!?]', text.strip())
+    sentences = [s for s in sentences if s.strip()]
+    return len(sentences)
