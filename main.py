796267cbbeb3626b04c28728a606c3cc38293333
Kate <katerynapavlenko01@gmail.com>
2026-04-08 14:03:55 +0300

    Add count_words function

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import re
 def read_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
         return f.read()
+    
+def count_words(text):
+    words = re.split(r'[ ,;:]+', text.strip())
+    words = [w for w in words if w]
+    return len(words)
